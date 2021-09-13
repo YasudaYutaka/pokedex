4d834e981c80ae328d10a68b57edb57f35af96d5
Rafael Yutaka Yasuda <62554340+YasudaYutaka@users.noreply.github.com>
2021-09-13 01:08:47 -0300

    Update app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import List from './components/List/List';
+import Header from './components/Header/Header';
+import styles from './Styles.module.scss';
+import SinglePokemon from './components/SinglePokemon/SinglePokemon';
 
+/**
+ * Main component to show all the components inside a <body>
+ * @returns a Header with the PokéDex logo, a Forms with input and select elements,
+ * along with a grid rendering the Pokémon list, wich shows a Modal when clicked.
+ */
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [pokemonModalIsShown, setPokemonModalIsShown] = useState(false);
+	const [pokemonName, setPokemonName] = useState('');
+	const [pokemonUrl, setPokemonUrl] = useState('');
+
+	const showPokemonModalHandler = (pokemon) => {
+		setPokemonModalIsShown(true);
+		setPokemonName(pokemon.name);
+		setPokemonUrl(pokemon.url);
+	};
+
+	const hidePokemonModalHandler = () => {
+		setPokemonModalIsShown(false);
+	};
+
+	return (
+		<div className={styles.container}>
+			{pokemonModalIsShown && (
+				<SinglePokemon
+					onClose={hidePokemonModalHandler}
+					pokemonName={pokemonName}
+					pokemonUrl={pokemonUrl}
+				/>
+			)}
+			<Header />
+			<List onShowPokemonModal={showPokemonModalHandler} />
+		</div>
+	);
 }
 
 export default App;
