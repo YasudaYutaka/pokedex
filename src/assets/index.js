f7231da813fa35a9948581ee0308ad90923ca265
Rafael Yutaka Yasuda <62554340+YasudaYutaka@users.noreply.github.com>
2021-09-13 01:05:37 -0300

    index.js

---
+++ src/assets/index.js
@@ -0,0 +1,41 @@
+import Bug from './img/pokemon-types/bug.png';
+import Dark from './img/pokemon-types/dark.png';
+import Dragon from './img/pokemon-types/dragon.png';
+import Electric from './img/pokemon-types/electric.png';
+import Fairy from './img/pokemon-types/fairy.png';
+import Fighting from './img/pokemon-types/fighting.png';
+import Fire from './img/pokemon-types/fire.png';
+import Flying from './img/pokemon-types/flying.png';
+import Ghost from './img/pokemon-types/ghost.png';
+import Grass from './img/pokemon-types/grass.png';
+import Ground from './img/pokemon-types/ground.png';
+import Ice from './img/pokemon-types/ice.png';
+import Normal from './img/pokemon-types/normal.png';
+import Poison from './img/pokemon-types/poison.png';
+import Psychic from './img/pokemon-types/psychic.png';
+import Rock from './img/pokemon-types/rock.png';
+import Steel from './img/pokemon-types/steel.png';
+import Water from './img/pokemon-types/water.png';
+
+const Types = {
+	Bug,
+	Dark,
+	Dragon,
+	Electric,
+	Fairy,
+	Fighting,
+	Fire,
+	Flying,
+	Ghost,
+	Grass,
+	Ground,
+	Ice,
+	Normal,
+	Poison,
+	Psychic,
+	Rock,
+	Steel,
+	Water,
+};
+
+export default Types;
